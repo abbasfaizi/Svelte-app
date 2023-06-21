69d8b4cae5d2ee247d4ec0a3e3248e6e8441b60b
Abbas Faizi <Abbas.Faizi@qlogic.se>
2023-06-21 16:51:29 +0200

    pass props with spread attributes

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 	import Library from "./Library/library.svelte";
 	import Detail from "./detail/Detail.svelte";
 
-	let page = 'library';
-	let pageArgs = {};
+	let page = 'detail';
+	let pageArgs = {id: 3};
 	function handleBookSelect(event){
 		page = 'detail';
 		pageArgs = event.detail;
@@ -16,7 +16,7 @@
 <!-- <Library on:book-select={handleBookSelect}/> -->
 
 {#if page === 'detail'}
-	<Detail />
+	<Detail {...pageArgs}/>
 	{:else}
 	<Library on:book-select={handleBookSelect}/>
 	{/if}
@@ -26,5 +26,9 @@
 <style>
 main {
 		padding: var(--spacingLarge);
+		;
+		padding: 1em;
+		
+		margin: 0 auto;
 	}
 </style>
