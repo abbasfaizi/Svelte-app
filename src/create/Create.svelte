287c7beb71ad3fbac4131a069ec4e4e39a766b5a
Abbas Faizi <Abbas.Faizi@qlogic.se>
2023-06-22 13:16:51 +0200

    finished the project of library

--- src/create/Create.svelte
+++ src/create/Create.svelte
@@ -1,20 +1,42 @@
 <script>
+    import { navigate } from "svelte-routing";
+  
     import BackButtonRow from "../common/BackButtonRow.svelte";
     import BookCover from "../common/BookCover.svelte";
     import Button from "../common/Button.svelte";
     import Header from "../common/Header.svelte";
     import { httpPost } from "../common/api.js";
-    let title = '';
-
-
-    
-
-    $: console.log({title});
+    import TextInput from "./TextInput.svelte";
+  
+    let title = "";
+    let author = "";
+    let cover = "";
+    let about = "";
+  
+    $: book = { title, author, cover, about };
+  
+    async function handleSubmit(event) {
+      function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+      const newBook = {
+        ...book,
+        variation: getRandomInt(0, 2),
+        favorite: false
+      };
+  
+      const { ok } = await httpPost("/", newBook);
+      if (ok) {
+        navigate("/");
+      }
+    }
   </script>
   
   <style>
     form {
-      /* display: grid; */
+      display: grid;
       grid-auto-rows: auto;
       grid-template-columns: 1fr;
       gap: var(--spacingXLarge);
@@ -29,9 +51,6 @@
       grid-template-columns: minmax(20vw, 10rem);
       grid-template-rows: minmax(32vw, 16rem);
     }
-    .d{
-        text-align: left;
-    }
     @media (min-width: 48rem) {
       form {
         grid-template-columns: 60vw 20vw;
@@ -43,19 +62,21 @@
   
   <Header element="h1" size="large">Create</Header>
   
-  <form>
+  <form on:submit|preventDefault={handleSubmit}>
     <div class="fields">
-        <label>
-            <span>Title</span>
-            <input type="text" bind:value={title} />
-        </label>
+      <TextInput label="Title" bind:value={title} />
+      <TextInput label="Author" bind:value={author} />
+      <TextInput label="Cover URL" bind:value={cover} />
+      <TextInput label="About" bind:value={about} multiline />
+      <div>
+        <Button>Save</Button>
+      </div>
     </div>
   
     <div>
-      <Header class="d">Preview</Header>
+      <Header>Preview</Header>
       <div class="preview">
+        <BookCover {book} />
       </div>
     </div>
-
-    <p>hello</p>
   </form>
